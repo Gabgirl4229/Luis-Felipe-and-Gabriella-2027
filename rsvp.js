427c6b63e4f2becdbd603060efac3c3ce65d04c4
Gabgirl4229 <67796364+Gabgirl4229@users.noreply.github.com>
2026-03-27 14:28:12 -0400

    Update rsvp.js

--- rsvp.js
+++ rsvp.js
@@ -2,4 +2,26 @@ function tryMe() {
   alert("testing testing 1 2 3");
 }
 
+function searchGuest(firstName, lastName) {
+}
+
+function showResults(results) {
+  if (results.length === 0) {
+    resultsContainer.innerHTML = "<p>No users found.</p>";
+    return;
+  }
+  const userCard = results.map(user => `
+    <div class="user-card">
+    <h3>${user.firstName + " " + user.lastName}</h3>
+    <p>Test Line 1</p>
+    <p>???: ${user.languagePreference}</p>
+    </div>
+  `).join(""); // Join array of strings into a single HTML string
+ 
+  resultsContainer.innerHTML = userCard;
+}
+
 document.getElementById("guestSearch").addEventListener("click", tryMe);
+//const guestName = document.getElementById("guestSearch").addEventListener("click", tryMe);
+//const results = document.getElementById("results");
+

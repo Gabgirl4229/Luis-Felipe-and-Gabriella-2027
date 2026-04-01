d6dfd5c64044c94d04a209d0d80b51df27f6301e
Gabgirl4229 <67796364+Gabgirl4229@users.noreply.github.com>
2026-04-01 13:08:37 -0400

    Update rsvp.js

--- rsvp.js
+++ rsvp.js
@@ -1,27 +1,7 @@
-function tryMe(firstName, lastName) {
-  alert("Hello " + firstName + " " + lastName + "!");
+function sayName(fname, lname) {
+  alert("Welcome, " + fname + " " + lname + "!");
 }
 
-function searchGuest(firstName, lastName) {
-}
-
-function showResults(results) {
-  if (results.length === 0) {
-    resultsContainer.innerHTML = "<p>No users found.</p>";
-    return;
-  }
-  const userCard = results.map(user => `
-    <div class="user-card">
-    <h3>${user.firstName + " " + user.lastName}</h3>
-    <p>Test Line 1</p>
-    <p>???: ${user.languagePreference}</p>
-    </div>
-  `).join(""); // Join array of strings into a single HTML string
- 
-  resultsContainer.innerHTML = userCard;
-}
-
-document.getElementById("guestSearch").addEventListener("click", tryMe(document.getElementById("fname"), document.getElementById("lname")));
-//const guestName = document.getElementById("guestSearch").addEventListener("click", tryMe);
-//const results = document.getElementById("results");
-
+fname = document.getElementById("fname");
+lname = document.getElementById("lname");
+document.getElementById("guestSearch").addEventListener("click", sayName(fname, lname));

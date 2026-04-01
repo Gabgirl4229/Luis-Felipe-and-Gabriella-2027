cde37a86fdb42505650ee07770393c559c8ba32d
Gabgirl4229 <67796364+Gabgirl4229@users.noreply.github.com>
2026-04-01 15:41:11 -0400

    Update rsvp.js

--- rsvp.js
+++ rsvp.js
@@ -1,11 +1,13 @@
 function searchGuest() {
   fname = document.getElementById("fname").value;
   lname = document.getElementById("lname").value;
-  alert("Welcome, " + fname + " " + lname + "!");
   document.getElementById("searchResult").innerHTML = "Welcome, " + fname + " " + lname + "!";
 }
 
-/*
+function tryMe() {
+  alert("YIPPEE");
+}
+
 function validateGuestName() {
   //const guests = JSON.parse(response)
   fetch('guests.json')
@@ -21,7 +23,8 @@ function validateGuestName() {
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
   });
+
+  tryMe();
 }
-*/
 
 //document.getElementById("guestSearch").addEventListener("click", sayName());

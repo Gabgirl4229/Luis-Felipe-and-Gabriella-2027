65a69951d4832f518f18552adc7f1b41f87d2271
Gabgirl4229 <67796364+Gabgirl4229@users.noreply.github.com>
2026-04-01 15:20:48 -0400

    Update rsvp.js

--- rsvp.js
+++ rsvp.js
@@ -2,7 +2,7 @@ function searchGuest() {
   fname = document.getElementById("fname").value;
   lname = document.getElementById("lname").value;
   alert("Welcome, " + fname + " " + lname + "!");
-  //document.getElementById("searchResult).innerHTML = "Welcome, " + fname + " " + lname + "!";
+  document.getElementById("searchResult").innerHTML = "Welcome, " + fname + " " + lname + "!";
 }
 
 /*

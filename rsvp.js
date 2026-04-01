362bb43e4f6af849f46528fedad31f643d985d9e
Gabgirl4229 <67796364+Gabgirl4229@users.noreply.github.com>
2026-04-01 14:51:32 -0400

    Update rsvp.js

--- rsvp.js
+++ rsvp.js
@@ -5,4 +5,21 @@ function searchGuest() {
   document.getElementById("searchResult).innerHTML = "Welcome, " + fname + " " + lname + "!";
 }
 
+function validateGuestName() {
+  //const guests = JSON.parse(response)
+  fetch('guests.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data); // Now you have the JSON data to work with
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
+}
+
 //document.getElementById("guestSearch").addEventListener("click", sayName());

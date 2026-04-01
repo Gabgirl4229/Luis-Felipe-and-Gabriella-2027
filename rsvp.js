20a8bfda48260f5d3abbe3b316be1768017dfd9e
Gabgirl4229 <67796364+Gabgirl4229@users.noreply.github.com>
2026-04-01 14:42:18 -0400

    Update rsvp.js

--- rsvp.js
+++ rsvp.js
@@ -1,4 +1,4 @@
-function sayName() {
+function searchGuest() {
   fname = document.getElementById("fname").value;
   lname = document.getElementById("lname").value;
   alert("Welcome, " + fname + " " + lname + "!");

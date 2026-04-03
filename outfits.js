c1399902d43aac1374ba23e9e46c91e820c50d3d
Gabgirl4229 <67796364+Gabgirl4229@users.noreply.github.com>
2026-04-03 15:46:22 -0400

    Update outfits.js

--- outfits.js
+++ outfits.js
@@ -1,28 +1,30 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndexes = [1, 1];
+showSlides(1,0);
+showSlides(1,1);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, num) {
+  showSlides(slideIndexes[num] += n, num);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, num) {
+  showSlides(slideIndexes[num] = n, num);
 }
 
-function showSlides(n) {
+function showSlides(n, num) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let containers = document.getElementsByClassName("slideshow-container");
+  let slides = containers[num].getElementsByClassName("mySlides");
+  let dots = containers[num].parentElement.querySelectorAll(".dot");
+  if (n > slides.length) {slideIndexes[num] = 1}
+  if (n < 1) {slideIndexes[num] = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndexes[num]-1].style.display = "block";
+  dots[slideIndexes[num]-1].className += " active";
 }

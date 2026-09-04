4b435748b5749d3ab22a2f1d4612d9e3c56be197
Gabgirl4229 <67796364+Gabgirl4229@users.noreply.github.com>
2026-09-04 17:42:11 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,11 +1,17 @@
-    // Allow nav bar to open and close
-    function toggleMenu() {
-      var x = document.getElementById("navLinks");
-      if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
-    }
-    // Enable smooth scrolling for anchor links
-    document.documentElement.style.scrollBehavior = 'smooth';
+// Allow nav bar to open and close
+function toggleMenu() {
+  var x = document.getElementById("navLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+// Close the menu when you jump forward to a different section of the same page
+function closeMenu() {
+    document.getElementById("navLinks").style.display = "none";
+}
+
+// Enable smooth scrolling for anchor links
+document.documentElement.style.scrollBehavior = 'smooth';

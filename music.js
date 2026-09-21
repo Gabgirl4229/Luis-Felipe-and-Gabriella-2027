a4bfbaca31ee95c74333e1d70d4da679a392af3a
Gabgirl4229 <67796364+Gabgirl4229@users.noreply.github.com>
2026-09-20 20:44:28 -0400

    Update music.js

--- music.js
+++ music.js
@@ -5,12 +5,12 @@ const icon = document.getElementById("speakerIcon");
 button.addEventListener("click", () => {
   if (audio.paused) {
     audio.play();
-    icon.src = "speaker-on.svg";
+    icon.src = "images/ui/speaker_on.PNG";
     icon.alt = "Audio on";
     button.setAttribute("aria-label", "Mute audio");
   } else {
     audio.pause();
-    icon.src = "speaker-off.svg";
+    icon.src = "images/ui/speaker_off.PNG";
     icon.alt = "Audio muted";
     button.setAttribute("aria-label", "Play audio");
   }
